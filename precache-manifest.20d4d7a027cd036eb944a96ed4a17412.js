965765881a5bc872d61f26d685f838e97aff5d5c
Juan Rebella <jrebella96@gmail.com>
2020-05-26 16:45:48 -0300

    Updates

---
+++ precache-manifest.20d4d7a027cd036eb944a96ed4a17412.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6dcf442d90bb2f621532e118e7fa711",
+    "url": "/reactjs-challenge/index.html"
+  },
+  {
+    "revision": "e36accb5b409fa7332f6",
+    "url": "/reactjs-challenge/static/css/2.0b53e794.chunk.css"
+  },
+  {
+    "revision": "bff3bbfbbbee5f4288ca",
+    "url": "/reactjs-challenge/static/css/main.b50566f2.chunk.css"
+  },
+  {
+    "revision": "e36accb5b409fa7332f6",
+    "url": "/reactjs-challenge/static/js/2.95160f6f.chunk.js"
+  },
+  {
+    "revision": "e326119bf971d925470072bdaaf2cb38",
+    "url": "/reactjs-challenge/static/js/2.95160f6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bff3bbfbbbee5f4288ca",
+    "url": "/reactjs-challenge/static/js/main.d5aeab5c.chunk.js"
+  },
+  {
+    "revision": "a0513a689b4b26b0909d",
+    "url": "/reactjs-challenge/static/js/runtime-main.b969ae3a.js"
+  }
+]);
